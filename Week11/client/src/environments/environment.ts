ad85b808c9f82000306dcddda0a124293a3112c1
Wenjie Zhou <jack-zwj@hotmail.com>
2023-07-26 16:46:00 -0400

    Firebase

---
+++ Week11/client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyCHJ1RqwTp_aDsrMUuiJ1PKKRSMTpgJOs0',
+        authDomain: 'comp229-1ddc0.firebaseapp.com',
+        databaseURL: 'https://comp229-1ddc0-default-rtdb.firebaseio.com',
+        projectId: 'comp229-1ddc0',
+        storageBucket: 'comp229-1ddc0.appspot.com',
+        messagingSenderId: '1084037501199',
+        appId: '1:1084037501199:web:5724b7db7c57a580406e7a'
+    }
+};
